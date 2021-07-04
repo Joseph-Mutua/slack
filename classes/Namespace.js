6145a33870e07768269e609a712d791396ccaf9e
Joseph Mutua <mutuaj793@gmail.com>
2021-07-04 14:22:17 +0300

    Addroom and namespace classes

---
+++ classes/Namespace.js
@@ -0,0 +1,15 @@
+class Namespace {
+  constructor(id, nsTitle, img, endpoint) {
+    this.id = id;
+    this.img = img;
+    this.nsTitle = nsTitle;
+    this.endpoint = endpoint;
+  }
+
+  addRoom(roomObj) {
+    this.rooms.push(roomObj);
+  }
+
+
+  
+}
